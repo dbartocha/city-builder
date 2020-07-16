efb92311bb8c55e3f5273bc89b4a1a714b3ee0db
Mateusz Boruszczak <borsuk1253@gmail.com>
2020-07-16 20:15:32 +0200

    Add sample files to organize files

---
+++ src/components/CityBuilder/CityBuilder.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import styles from './CityBuilder.module.scss';
+
+class CityBuilder extends Component {
+  render() {
+    return (
+      <div className={styles.CityBuilder}>Builder</div>
+    );
+  };
+}
+
+export default CityBuilder
---
+++ src/hoc/Layout/Layout.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Layout extends Component {
+  render() {
+    return (
+      <div>Layout {this.props.children}</div>
+    );
+  };
+}
+
+export default Layout;
