31c761a1c0abea7565ea311a0d1089914651808c
Mateusz Boruszczak <borsuk1253@gmail.com>
2020-07-19 13:40:44 +0200

    Add board generator

--- src/components/CityBuilder/CityBuilder.js
+++ src/components/CityBuilder/CityBuilder.js
@@ -1,10 +1,44 @@
 import React, { Component } from 'react';
+import SingleField from '../SingleField/SingleField';
+
 import styles from './CityBuilder.module.scss';
 
+
 class CityBuilder extends Component {
+  state = {
+    gameBoard: {
+      rows: 4,
+      columns: 3,
+    },
+  }
+
   render() {
+    let rows = [];
+    let boardArray = [];
+    for(let i = 0; i<this.state.gameBoard.rows; i++) {
+      for(let j = 0; j<this.state.gameBoard.columns; j++) {
+        rows.push(j)
+      }
+      boardArray.push(rows);
+      rows = [];
+    }
+
+    const gameBoard = boardArray.map((row, rowIndex) => {
+      return (
+        <div key={rowIndex}>
+          {
+            row.map((_, colIndex) => {
+              return <SingleField key={colIndex}/>;
+            })
+          }
+        </div>
+      )
+    })
+
     return (
-      <div className={styles.CityBuilder}>Builder</div>
+      <div className={styles.CityBuilder}>
+        {gameBoard}
+      </div>
     );
   };
 }
---
+++ src/components/SingleField/SingleField.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './SingleField.module.scss';
+
+const singleField = () => (
+  <div className={styles.SingleField}>
+
+  </div>
+);
+
+export default singleField;
